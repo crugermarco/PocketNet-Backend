6d24f64616e33654a40ca85f03d3cd434343f921
Cruger <marcocrugeruchija@gmail.com>
2025-12-15 10:11:06 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import PocketNetStore from './components/PocketNetStore';
-import Success from './Success';
+import Success from './components/Success'; 
 
 function App() {
+  console.log('✅ App montada - Router configurado');
+  
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<PocketNetStore />} />
-        <Route path="/success" element={<Success />} />
-      </Routes>
+      <div style={{ minHeight: '100vh' }}>
+        <Routes>
+          <Route path="/" element={<PocketNetStore />} />
+          <Route path="/success" element={<Success />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
